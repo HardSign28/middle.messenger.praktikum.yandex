8b9b6e82f6d04ff16c6a388e1d1ed0d64097fb33
Vladislav Zagvostkin <vzagvostkin@cic.kz>
2025-02-12 18:52:06 +0500

    Back Button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { Button } from './button'
+export { BackButton } from './back-button'
 export { Input } from './input'
 export { ListCat } from './list-cats'
 export { CatCard } from './cat-card'
