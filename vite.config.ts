ff1c7f91102c019c1ff716c54e4c34c1b96b29c3
hardsign28 <tz-vladislav@yandex.kz>
2025-02-13 18:13:02 +0500

    alias, fix build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 
 export default defineConfig({
     root: '.',
     server: {
         port: 3000,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
     },
     build: {
         outDir: 'dist'
