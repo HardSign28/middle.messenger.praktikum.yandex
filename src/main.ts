b6e91230f1fd195634623889001999c5e988f6f2
hardsign28 <tz-vladislav@yandex.kz>
2025-02-13 16:17:35 +0500

    404 страница, fix typescript errors

--- src/main.ts
+++ src/main.ts
@@ -10,27 +10,28 @@ import avatar from './assets/icons/avatar.svg?raw';
 
 Handlebars.registerHelper('eq', (a, b) => a === b);
 Handlebars.registerHelper('defaultAvatar', () => avatar);
-const pages = {
-  'login': [ Pages.LoginPage ],
-  'register': [ Pages.RegisterPage ],
-  'profile': [ Pages.ProfilePage, { img: avatar, cat1 }],
-  'list': [ Pages.ListPage, {
+const pages: Record<string, [string, Record<string, unknown>?]> = {
+  login: [Pages.LoginPage],
+  register: [Pages.RegisterPage],
+  profile: [Pages.ProfilePage, { img: avatar, cat1 }],
+  list: [Pages.ListPage, {
     cats: [
-      {name: 'cat-1', avatar: cat1},
-      {name: 'cat-2', avatar: cat2, active: true},
-      {name: 'cat-3', avatar: cat3},
+      { name: 'cat-1', avatar: cat1 },
+      { name: 'cat-2', avatar: cat2, active: true },
+      { name: 'cat-3', avatar: cat3 },
     ],
     showDialog: true
   }],
-  'nav': [ Pages.NavigatePage ]
+  404: [Pages.NotFoundPage],
+  nav: [Pages.NavigatePage]
 };
 
 Object.entries(Components).forEach(([ name, template ]) => {
   Handlebars.registerPartial(name, template);
 });
 
-function navigate(page: string) {
-  //@ts-ignore
+function navigate(page: keyof typeof pages) {
+
   const [ source, context ] = pages[page];
   const container = document.getElementById('app')!;
 
@@ -41,12 +42,14 @@ function navigate(page: string) {
   container.innerHTML = templatingFunction(fullContext);
 }
 
-document.addEventListener('DOMContentLoaded', () => navigate('profile'));
+document.addEventListener('DOMContentLoaded', () => navigate('register'));
 
 document.addEventListener('click', e => {
-  //@ts-ignore
-  const page = e.target.getAttribute('page');
-  if (page) {
+  const target = e.target as HTMLElement | null;
+
+  if (target && target.hasAttribute('page')) {
+    const page = target.getAttribute('page') as keyof typeof pages;
+
     navigate(page);
 
     e.preventDefault();
