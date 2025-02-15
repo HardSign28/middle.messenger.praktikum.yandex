bdf6524cbd6fd092e0a31748f6f03b6b78ceb21a
hardsign28 <tz-vladislav@yandex.kz>
2025-02-15 23:09:00 +0500

    Delete List

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
 export { LoginPage } from './login';
 export { RegisterPage } from './register';
 export { ProfilePage } from './profile';
-export { ListPage } from './list';
 export { ChatPage } from './chat';
 export { NavigatePage } from './navigate';
 export { NotFoundPage } from './404';
